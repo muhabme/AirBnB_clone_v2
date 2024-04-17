ff1438d0f40e3ce41bf4f69a3c7942a4bf225d42
MuhabGamal <muhabme@gmail.com>
2024-04-18 00:57:44 +0200

    DBStorage - States and Cities

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,7 +2,24 @@
 """ DBStorage Module for HBNB project """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from os import getenv
+import os
+from models.base_model import Base
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.user import User
+
+# Mapping from class names to classes
+class_name_map = {
+    'Amenity': Amenity,
+    'City': City,
+    'Place': Place,
+    'State': State,
+    'Review': Review,
+    'User': User
+}
 
 class DBStorage:
     """This class manages storage of hbnb models in a MySQL database."""
@@ -11,39 +28,40 @@ class DBStorage:
 
     def __init__(self):
         """Initialize the DBStorage instance."""
-        user = getenv('HBNB_MYSQL_USER')
-        password = getenv('HBNB_MYSQL_PWD')
-        host = getenv('HBNB_MYSQL_HOST')
-        db = getenv('HBNB_MYSQL_DB')
-        env = getenv('HBNB_ENV')
-
+        user = os.getenv('HBNB_MYSQL_USER')
+        passwd = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST')
+        db = os.getenv('HBNB_MYSQL_DB')
+        
         # Database engine setup
-        self.__engine = create_engine(f'mysql+mysqldb://{user}:{password}@{host}/{db}',
+        self.__engine = create_engine(f'mysql+mysqldb://{user}:{passwd}@{host}/{db}',
                                       pool_pre_ping=True)
         
         # Drop all tables if in test environment
-        if env == 'test':
+        if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """Query all objects in the database by class name; if no class specified, query all."""
-        from models.base_model import Base
-        from sqlalchemy.orm import joinedload
+        if cls and isinstance(cls, str):
+            cls = class_name_map.get(cls, None)
         result_dict = {}
         if cls:
-            objs = self.__session.query(cls).all()
-            for obj in objs:
+            for obj in self.__session.query(cls):
                 key = f"{obj.__class__.__name__}.{obj.id}"
                 result_dict[key] = obj
         else:
-            # List all classes that inherit from Base
-            classes = Base._decl_class_registry.values()
-            for cls in classes:
-                objs = self.__session.query(cls).options(joinedload('*')).all()
-                for obj in objs:
+            for model_cls in class_name_map.values():
+                for obj in self.__session.query(model_cls):
                     key = f"{obj.__class__.__name__}.{obj.id}"
                     result_dict[key] = obj
         return result_dict
+
+    def reload(self):
+        """Create all tables in the database and initialize a new session."""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(session_factory)
 
     def new(self, obj):
         """Add the object to the current database session."""
@@ -58,11 +76,23 @@ class DBStorage:
         if obj:
             self.__session.delete(obj)
 
-    def reload(self):
-        """Create all tables in the database and initialize a new session."""
-        from models.base_model import Base
-        # Import all necessary classes here
-        Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        ScopedSession = scoped_session(session_factory)
-        self.__session = ScopedSession()
+    def close(self):
+        """Dispose of current session if active"""
+        self.__session.remove()
+
+    def get(self, cls, id):
+        """Retrieve an object by class and id."""
+        if cls and isinstance(cls, str) and id:
+            cls = class_name_map.get(cls, None)
+            return self.__session.query(cls).filter(cls.id == id).first()
+        return None
+
+    def count(self, cls=None):
+        """Count number of objects in storage by class, or total if no class specified."""
+        if cls and isinstance(cls, str):
+            cls = class_name_map.get(cls, None)
+            return self.__session.query(cls).count()
+        elif cls is None:
+            total = sum(self.__session.query(model_cls).count() for model_cls in class_name_map.values())
+            return total
+        return 0
