3eb33ff349889238abe16c613bde16a4df3ece5b
MuhabGamal <muhabme@gmail.com>
2024-04-18 01:22:51 +0200

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,7 @@ class BaseModel:
             if key == '__class__':
                 continue
             if key == "created_at" or key == "updated_at":
-                value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                value = datetime.strptime(value, "%Y-%m-%d %H:%M:%S.%f")
             setattr(self, key, value)
 
     def __str__(self):
