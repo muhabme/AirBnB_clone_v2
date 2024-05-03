1bc6809eaba5d343a82833392d8a879fb65db580
MuhabGamal <muhabme@gmail.com>
2024-05-04 02:33:35 +0300

    changes

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers
-"""
-
+"""A module for web application deployment with Fabric."""
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
@@ -50,4 +46,3 @@ def deploy():
     if archive_path is None:
         return False
     return do_deploy(archive_path)
-
